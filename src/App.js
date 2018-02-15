840c5a073d3981e5ddc7ba231ea05c199a21f162
Nick Sarafa <nicksarafa@gmail.com>
2018-02-15 19:29:58 +0800

    injectglobal styled-normalized init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
+import styled, { injectGlobal } from 'styled-components'
+import styledNormalize from 'styled-normalize'
+
+/**
+ * globally injected style scripts. will eventually replace App/index.css
+ * @see https://www.styled-components.com/docs/api#injectglobal
+ */
+injectGlobal`
+  ${styledNormalize}
+`
+
 class App extends Component {
   render() {
     return (
       <div>
-      hello, world
       </div>
     )
   }
